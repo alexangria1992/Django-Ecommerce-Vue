5ed4b6a59cd0fa292ecbb2fedd30705017d0a887
alexangria1992 <alex040892@gmail.com>
2020-12-13 15:17:53 +1300

    Created virtual env, django, migrations, superuser, apps folder, base template, and view routing for homepage

---
+++ ecommerce/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib import admin
+from apps.core.views import frontpage
+
+urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('admin/', admin.site.urls),
+]
