bada8ca471236902583621272d64a94443a557e1
alexangria1992 <alex040892@gmail.com>
2020-12-13 16:24:10 +1300

    Initiated Store App, got list of products to show on frontpage

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib import admin
-from apps.core.views import frontpage
+from apps.core.views import frontpage, contact
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
