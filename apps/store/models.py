0b1a534fbd3b1f4305cdc16b6f353f814e5beb36
alexangria1992 <alex040892@gmail.com>
2020-12-13 16:52:48 +1300

    Detail view for product and categories in navbar

--- apps/store/models.py
+++ apps/store/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField( max_digits = 5, 
-                         decimal_places = 2)
+                         decimal_places = 2 )
 
     def __str__(self):
       return self.title
--- apps/store/views.py
+++ apps/store/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {
+        'product':product
+    }
+    return render(request, 'product_detail.html', context)
