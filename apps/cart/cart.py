06c4ce3f532ed1eee604e21b2c16cc6e06b61d9e
alexangria1992 <alex040892@gmail.com>
2020-12-16 21:45:51 +1300

    Fixed Bug, Show quantity and price in cart, update quantity, automatically remove products,  cart quantity in navbar

--- apps/cart/cart.py
+++ apps/cart/cart.py
@@ -38,7 +38,8 @@ class Cart(object):
             self.cart[product_id]['quantity'] = quantity 
         else:
              self.cart[product_id]['quantity'] = self.cart[product_id]['quantity'] + 1
-             self.save()
+        
+        self.save()
 
     def remove(self, product_id):
         if  product_id in self.cart:
@@ -49,3 +50,6 @@ class Cart(object):
         print('save')
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
+
+def get_total_length(self):
+    return sum(int(item['quantity']) for item in self.cart.values())
